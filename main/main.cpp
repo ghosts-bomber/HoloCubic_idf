37e5a75dbc3679666a9fe2a8d285d63df907456d
codeM <maxinyuan@neolix.cn>
2024-07-16 17:12:40 +0800

    init

---
+++ main/main.cpp
@@ -0,0 +1,21 @@
+
+#include "nvs.h"
+#include <esp_log.h>
+#include <esp_err.h>
+#include <nvs_flash.h>
+static const char *TAG = "main";
+
+static void init_nvs() {
+  esp_err_t ret = nvs_flash_init();
+  if(ret == ESP_ERR_NVS_NO_FREE_PAGES || ret==ESP_ERR_NVS_NEW_VERSION_FOUND){
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+
+}
+extern "C" void app_main(void) { 
+  esp_log_level_set(TAG, ESP_LOG_INFO); 
+  init_nvs(); 
+}
+  
